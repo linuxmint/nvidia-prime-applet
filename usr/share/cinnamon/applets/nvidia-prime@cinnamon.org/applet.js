be482620087e4b1707b568019482bfeee10bc194
NikoKrause <nikokrause@gmx.de>
2017-12-13 17:55:41 +0100

    applet: use symbolic nvidia/intel icons
    resize them as fullcolor icons, else they look too small

--- usr/share/cinnamon/applets/nvidia-prime@cinnamon.org/applet.js
+++ usr/share/cinnamon/applets/nvidia-prime@cinnamon.org/applet.js
@@ -11,11 +11,11 @@ function MyApplet(metadata, orientation, panel_height, instance_id) {
 }
 
 MyApplet.prototype = {
-    __proto__: Applet.TextIconApplet.prototype,
+    __proto__: Applet.IconApplet.prototype,
 
     _init: function(metadata, orientation, panel_height, instance_id) {
-        Applet.TextIconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
-        
+        Applet.IconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
+
         try {
             this.metadata = metadata;
             Main.systrayManager.registerRole("nvidia-prime", metadata.uuid);
@@ -23,17 +23,17 @@ MyApplet.prototype = {
                 if (this._doCommand(["prime-supported"]) == "yes") {
                     let active_gpu = this._doCommand(["prime-select", "query"]);
                     if (active_gpu == "nvidia") {
-                        this.set_applet_icon_name("prime-applet-nvidia");
+                        this.set_applet_icon_symbolic_name("prime-applet-nvidia");
                         this.set_applet_tooltip(_("Active graphics card: NVIDIA"));
                     }
                     else if (active_gpu == "intel") {
-                        this.set_applet_icon_name("prime-applet-intel");
+                        this.set_applet_icon_symbolic_name("prime-applet-intel");
                         this.set_applet_tooltip(_("Active graphics card: Intel"));
                     }
                     else {
                         this.set_applet_icon_symbolic_name("dialog-error");
                         this.set_applet_tooltip(_("Active graphics card: " + active_gpu));
-                    }           
+                    }
                 }
                 else {
                     this.set_applet_icon_symbolic_name("dialog-error");
@@ -44,7 +44,7 @@ MyApplet.prototype = {
                 this.menu = new Applet.AppletPopupMenu(this, orientation);
                 this.menuManager.addMenu(this.menu);
 
-                this.actor.add_style_class_name('panel-status-button');
+                //this.actor.add_style_class_name('panel-status-button');
 
                 this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
                 this.menu.addAction(_("NVIDIA Settings"), Lang.bind(this, function() {
@@ -55,6 +55,7 @@ MyApplet.prototype = {
                 this.set_applet_icon_symbolic_name("dialog-error");
                 this.set_applet_tooltip(_("The NVIDIA drivers are not properly installed. Please install nvidia-settings and nvidia-prime."));
             }
+            this.on_panel_height_changed();
         }
         catch (e) {
             global.logError(e);
@@ -62,7 +63,7 @@ MyApplet.prototype = {
     },
 
     _doCommand: function(command) {
-        [res,pid,fdin,fdout,fderr] = GLib.spawn_async_with_pipes(null, command, null, GLib.SpawnFlags.SEARCH_PATH, null);
+        let [res,pid,fdin,fdout,fderr] = GLib.spawn_async_with_pipes(null, command, null, GLib.SpawnFlags.SEARCH_PATH, null);
         let outstream = new Gio.UnixInputStream({fd:fdout,close_fd:true});
         let stdout = new Gio.DataInputStream({base_stream: outstream});
 
@@ -75,7 +76,13 @@ MyApplet.prototype = {
             return out.toString();
         }
     },
-    
+
+    on_panel_height_changed: function() {
+        let icon_name = this._applet_icon.get_icon_name();
+        if (icon_name == "prime-applet-intel" || icon_name == "prime-applet-nvidia")
+            this._applet_icon.set_icon_size(this._panelHeight-8);
+    },
+
     on_applet_clicked: function(event) {
         this.menu.toggle();
     },
